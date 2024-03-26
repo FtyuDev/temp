06778af62c10b367e8ee88e74752e79b31e39ac1
otabekoripov <148060763+otabekoripov009@users.noreply.github.com>
2024-03-26 23:23:17 +0500

    initial commit

--- pro/settings.py
+++ pro/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 
 # Add this in your settings.py file:
 STATICFILES_DIRS = [
-    BASE_DIR / 'mystaticfiles'
+    BASE_DIR / 'static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
